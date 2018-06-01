122c8efaf616136d53f94205f4194eb092bc3e6a
amittovim <37749433+amittovim@users.noreply.github.com>
2018-06-01 16:41:53 +0300

    WIP (#2)

--- src/logic/main.js
+++ src/logic/main.js
@@ -1,11 +1,13 @@
 import initPlayers from 'init/players.init';
 import initDrawPile from 'init/draw-pile.init';
+import initDiscardPile from 'init/discard-pile.init';
 
 // ===== Game init functions =====
 
 export function initGame() {
     initPlayers();
     initDrawPile();
+    initDiscardPile();
 }
 
 

971a9fdd0cb06f170fbc3b8a57049501dd5a2b3d
amit tovim <amit.tovim@gmail.com>
2018-06-10 08:27:33 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./style.css";
-import Game from "./app/taki/game.component";
-import * as gameService from './app/taki/game.service';
+import Game from "./app/game/game.component";
+import * as gameService from './app/game/game.service';
 
 const App = () => {
     return (<Game service={gameService} />);
